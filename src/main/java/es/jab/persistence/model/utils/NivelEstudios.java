697dbbffe2bb8f4d0c2e3afa020e128ea0fd123b
Javier Combarros <Javier@138.4.192.251>
2015-03-09 22:25:43 +0100

    NivelEstudios enum updated

--- src/main/java/es/jab/persistence/model/utils/NivelEstudios.java
+++ src/main/java/es/jab/persistence/model/utils/NivelEstudios.java
@@ -1,5 +1,26 @@
 package es.jab.persistence.model.utils;
 
 public enum NivelEstudios {
+	
+	PRIMARIA(1,"Educación Primaria"),
+	SECUNDARIA(2, "Educación Secundaria"),
+	FP(2, "Formación Profesional"),
+	UNIVERSITARIA(4,"Educación Universitaria");
+		
+	private int id;
+	
+	private String descripcion;
+	
+	private NivelEstudios (int id, String descripcion){
+		this.id = id;
+		this.descripcion = descripcion;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
 }
