5cd7d1329fea90e5adea5400ff5284f509731c1e
Javier Combarros <Javier@138.4.192.251>
2015-03-11 23:35:48 +0100

    TemaDaoJdbc class modified. delete, update, findAll methods added and modified

--- src/main/java/es/jab/persistence/model/dao/jdbc/TemaDaoJdbc.java
+++ src/main/java/es/jab/persistence/model/dao/jdbc/TemaDaoJdbc.java
@@ -2,6 +2,7 @@ package es.jab.persistence.model.dao.jdbc;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
@@ -37,23 +38,33 @@ public class TemaDaoJdbc extends GenericDaoJdbc<Tema, Integer> implements TemaDa
         return this.create(resultSet);
     }
 
-	@Override
-	public void update(Tema entity) {
-		// TODO Auto-generated method stub
-		
-	}
+	private static final String SQL_UPDATE = "UPDATE %s SET %s='%s', %s='%s', %s='%s', %s='%s', %s=%d WHERE ID=%d";
 
-	@Override
-	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void update(Tema tema) {
+        this.updateSql(String.format(SQL_UPDATE, Tema.TABLE,Tema.NOMBRE, tema.getNombre(), Tema.PREGUNTA,
+                tema.getPregunta(), tema.getId()));
+    }
 
-	@Override
-	public List<Tema> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void deleteById(Integer id) {
+        Tema temaBD = this.read(id);
+        if (temaBD != null) { 
+            this.updateSql(String.format(SQL_DELETE_ID, Tema.TABLE, id));
+        }
+    }
+
+    @Override
+    public List<Tema> findAll() {
+        List<Tema> list = new ArrayList<Tema>();
+        ResultSet resultSet = this.query(String.format(SQL_SELECT_ALL, Tema.TABLE));
+        Tema tema = this.create(resultSet);
+        while (tema != null) {
+            list.add(tema);
+            tema = this.create(resultSet);
+        }
+        return list;
+    }
 	
 	private Tema create(ResultSet resultSet) {
         Tema tema;
