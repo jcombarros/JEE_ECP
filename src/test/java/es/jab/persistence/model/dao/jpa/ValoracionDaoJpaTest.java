45bb42c0cf4a2eb79822a995cc2b9ac1a68ed2c3
Javier Combarros <Javier@138.4.192.251>
2015-03-11 21:58:19 +0100

    ValoracionDaoJpaTest class modified. Basic tests added.

--- src/test/java/es/jab/persistence/model/dao/jpa/ValoracionDaoJpaTest.java
+++ src/test/java/es/jab/persistence/model/dao/jpa/ValoracionDaoJpaTest.java
@@ -1,8 +1,12 @@
 package es.jab.persistence.model.dao.jpa;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import es.jab.persistence.model.dao.DaoFactory;
 import es.jab.persistence.model.dao.ValoracionDao;
@@ -27,6 +31,35 @@ public class ValoracionDaoJpaTest {
         this.valoracion = new Valoracion(new Tema("Pregunta 1", "¿Pregunta 1?"), NivelEstudios.UNIVERSITARIA, 8);
         valoracionDao = DaoFactory.getInstance().getValoracionDao();
         valoracionDao.create(valoracion);
+    }
+	
+	@Test
+    public void testRead() {
+		Valoracion v = valoracionDao.read(valoracion.getId());
+        assertEquals(valoracion, valoracionDao.read(valoracion.getId()));
+    }
+	
+	@Test
+    public void testUpdate() {
+		valoracion.setTema(new Tema("Pregunta 2", "¿Pregunta 2?"));
+		valoracion.setRespuesta(4);
+		valoracion.setNivelEstudios(NivelEstudios.PRIMARIA);
+        valoracionDao.update(valoracion);
+        assertEquals(valoracion, valoracionDao.read(valoracion.getId()));
+    }
+	
+	@Test
+    public void testDeleteByID() {
+		valoracionDao.deleteById(valoracion.getId());
+        assertNull(valoracionDao.read(valoracion.getId()));
+    }
+	
+	@Test
+    public void testFindAll() {
+        this.valoracion = new Valoracion(new Tema("Pregunta 3", "¿Pregunta 3?"),NivelEstudios.SECUNDARIA, 10);
+        valoracionDao = DaoFactory.getInstance().getValoracionDao();
+        valoracionDao.create(valoracion);
+        assertEquals(2, valoracionDao.findAll().size());
     }
 	
 	@After
