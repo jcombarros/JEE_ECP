7002f5f0c6b08380ad6e2f1c54294bfe89370b75
Javier Combarros <Javier@138.4.192.251>
2015-03-14 10:26:05 +0100

    Added index.html, Dispatcher & home.jsp Solved some problems with pom.xml file

---
+++ src/main/java/es/jab/view/beans/Dispatcher.java
@@ -0,0 +1,43 @@
+package es.jab.view.beans;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Dispatcher
+ */
+@WebServlet("/Votaciones/*")
+public class Dispatcher extends HttpServlet {
+	
+	private static String PATH_ROOT_VIEW = "/votaciones/";
+	
+	private static final long serialVersionUID = 1L;
+       
+    public Dispatcher() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getPathInfo().substring(1);
+
+        String view;
+        
+        switch (action) {
+	        case "other":
+	            view = action;
+	            break;
+	        default:
+	            view = "home";
+        }
+        this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
