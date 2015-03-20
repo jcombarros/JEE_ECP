41adfdfcb7310b24240e5c6954d1779935c916bd
Javier Combarros <Javier@138.4.192.251>
2015-03-20 19:29:58 +0100

    BorraTemaView class modified.Properties and Constructor added.

--- src/main/java/es/jab/view/beans/BorraTemaView.java
+++ src/main/java/es/jab/view/beans/BorraTemaView.java
@@ -1,5 +1,64 @@
 package es.jab.view.beans;
 
-public class BorraTemaView {
+import java.io.Serializable;
+import java.util.List;
+
+import es.jab.persistence.model.entities.Tema;
+
+public class BorraTemaView implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String mensaje;
+	
+	private String identificador;
+	
+	private List<Tema> temas;
+	
+	private Tema tema;
+	
+	public BorraTemaView(){
+		
+	}
+	
+	public BorraTemaView(Tema tema){
+		this.tema = tema;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+
+	public List<Tema> getTemas() {
+		return temas;
+	}
+
+	public void setTemas(List<Tema> temas) {
+		this.temas = temas;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	
+	public void update(){
+		
+	}
 
 }
