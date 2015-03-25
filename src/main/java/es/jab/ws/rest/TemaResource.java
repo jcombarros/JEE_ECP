c78c2070203e612e68b2de6eab54f17271d51306
Javier Combarros <Javier@138.4.192.251>
2015-03-25 22:43:31 +0100

    Annotations added to Resources

--- src/main/java/es/jab/ws/rest/TemaResource.java
+++ src/main/java/es/jab/ws/rest/TemaResource.java
@@ -2,32 +2,54 @@ package es.jab.ws.rest;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import es.jab.persistence.model.entities.Tema;
 
+@Path("/temas")
 public class TemaResource {
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
 	 public Response create(Tema entity) {
 		 return null;
 		 
 	 }
 	 
-	 public Tema read(int id) {
+	@GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_XML)
+	 public Tema read(@PathParam("id") int id) {
 		 return null;
 		 
 	 }
 	 
-	 public Response update(int id) {
+	@PUT
+    @Consumes(MediaType.APPLICATION_XML)
+	 public Response update(Tema entity) {
 		 return null;
 		 
 	 }
 	 
-	 public Response deleteById(int id) {
+	@DELETE
+    @Path("/{id}")
+	@Consumes(MediaType.APPLICATION_XML)
+	 public Response deleteById(@PathParam("id") int id) {
 		 return null;
 		 
 	 }
 	 
+	@GET
+    @Produces(MediaType.APPLICATION_XML)
 	 public List<Tema> findAll() {
 		 return null;
 		 
--- src/main/java/es/jab/ws/rest/ValoracionResource.java
+++ src/main/java/es/jab/ws/rest/ValoracionResource.java
@@ -2,41 +2,68 @@ package es.jab.ws.rest;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import es.jab.persistence.model.entities.Valoracion;
 
+@Path("/valoraciones")
 public class ValoracionResource {
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
 	public Response create(Valoracion entity) {
 		 return null;
 		 
 	 }
 	 
+	@GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_XML)
 	 public Valoracion read(int id) {
 		 return null;
 		 
 	 }
 	 
-	 public Response update(int id) {
+	@PUT
+    @Consumes(MediaType.APPLICATION_XML)
+	 public Response update(Valoracion entity) {
 		 return null;
 		 
 	 }
 	 
+	@DELETE
+    @Path("/{id}")
+	@Consumes(MediaType.APPLICATION_XML)
 	 public Response deleteById(int id) {
 		 return null;
 		 
 	 }
 	 
+	@GET
+    @Produces(MediaType.APPLICATION_XML)
 	 public List<Valoracion> findAll() {
 		 return null;
 		 
 	 }
 	 
+	@DELETE
+    @Path("/{id}" + "/byTema")
+	@Consumes(MediaType.APPLICATION_XML)
 	 public Response deleteByTema(int id){
 		 return null;
 	 }
 	 
+	@GET
+	@Path("byTema")
+    @Produces(MediaType.APPLICATION_XML)
 	 public List<Valoracion> getByTema(int id){
 		 return null;
 	 }
