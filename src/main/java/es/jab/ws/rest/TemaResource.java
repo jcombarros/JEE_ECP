dd7875323615f3325e3ceaabc47c7ee2302b6b8a
Javier Combarros <Javier@138.4.192.251>
2015-03-25 22:23:48 +0100

    TemaResource & ValoracionResource added. Methods head added

---
+++ src/main/java/es/jab/ws/rest/TemaResource.java
@@ -0,0 +1,36 @@
+package es.jab.ws.rest;
+
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
+import es.jab.persistence.model.entities.Tema;
+
+public class TemaResource {
+	
+	 public Response create(Tema entity) {
+		 return null;
+		 
+	 }
+	 
+	 public Tema read(int id) {
+		 return null;
+		 
+	 }
+	 
+	 public Response update(int id) {
+		 return null;
+		 
+	 }
+	 
+	 public Response deleteById(int id) {
+		 return null;
+		 
+	 }
+	 
+	 public List<Tema> findAll() {
+		 return null;
+		 
+	 }
+
+}
---
+++ src/main/java/es/jab/ws/rest/ValoracionResource.java
@@ -0,0 +1,44 @@
+package es.jab.ws.rest;
+
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
+import es.jab.persistence.model.entities.Valoracion;
+
+public class ValoracionResource {
+	
+	public Response create(Valoracion entity) {
+		 return null;
+		 
+	 }
+	 
+	 public Valoracion read(int id) {
+		 return null;
+		 
+	 }
+	 
+	 public Response update(int id) {
+		 return null;
+		 
+	 }
+	 
+	 public Response deleteById(int id) {
+		 return null;
+		 
+	 }
+	 
+	 public List<Valoracion> findAll() {
+		 return null;
+		 
+	 }
+	 
+	 public Response deleteByTema(int id){
+		 return null;
+	 }
+	 
+	 public List<Valoracion> getByTema(int id){
+		 return null;
+	 }
+
+}
