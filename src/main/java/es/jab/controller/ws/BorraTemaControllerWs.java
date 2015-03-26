6a252549dceaf2c8f2c1ec8125ccd147d1710635
Javier Combarros <Javier@138.4.192.251>
2015-03-26 20:18:58 +0100

    BorraTemaControllerWs class modified

--- src/main/java/es/jab/controller/ws/BorraTemaControllerWs.java
+++ src/main/java/es/jab/controller/ws/BorraTemaControllerWs.java
@@ -2,6 +2,8 @@ package es.jab.controller.ws;
 
 import java.util.List;
 
+import javax.ws.rs.core.GenericType;
+
 import es.jab.controller.BorraTemaController;
 import es.jab.persistence.model.entities.Tema;
 
@@ -9,19 +11,18 @@ public class BorraTemaControllerWs implements BorraTemaController {
 
 	@Override
 	public List<Tema> recuperarTemas() {
-		// TODO Auto-generated method stub
-		return null;
+		GenericType <List<Tema>> genericType = new GenericType<List<Tema>>() {};
+		return ControllerWs.buildWebServiceManager("/temas").entities(genericType);
 	}
 
 	@Override
 	public Tema recuperarTemaPorId(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return ControllerWs.buildWebServiceManager("/temas", String.valueOf(id)).entity(Tema.class);
 	}
 
 	@Override
 	public void borrarTema(int id) {
-		// TODO Auto-generated method stub
+		ControllerWs.buildWebServiceManager("/temas", String.valueOf(id)).delete();
 
 	}
 
