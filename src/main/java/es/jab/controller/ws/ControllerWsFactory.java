a76719b19960f315d99b7dcd890aa28cf53499bf
Javier Combarros <Javier@138.4.192.251>
2015-03-26 18:56:12 +0100

    ControllerWsFactory class added

---
+++ src/main/java/es/jab/controller/ws/ControllerWsFactory.java
@@ -0,0 +1,35 @@
+package es.jab.controller.ws;
+
+import es.jab.controller.BorraTemaController;
+import es.jab.controller.ControllerFactory;
+import es.jab.controller.NuevoTemaController;
+import es.jab.controller.VerTemasController;
+import es.jab.controller.VerVotacionesController;
+
+public class ControllerWsFactory extends ControllerFactory {
+
+	@Override
+	public VerTemasController getVerTemasController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NuevoTemaController getNuevoTemaController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BorraTemaController getBorraTemaController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VerVotacionesController getVerVotacionesController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
