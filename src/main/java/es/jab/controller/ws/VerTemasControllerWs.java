6d3d41def1ef449fe5798864c4cf596cdb4e701c
Javier Combarros <Javier@138.4.192.251>
2015-03-26 20:24:46 +0100

    VerTemasControllerWs class modified

--- src/main/java/es/jab/controller/ws/VerTemasControllerWs.java
+++ src/main/java/es/jab/controller/ws/VerTemasControllerWs.java
@@ -2,6 +2,8 @@ package es.jab.controller.ws;
 
 import java.util.List;
 
+import javax.ws.rs.core.GenericType;
+
 import es.jab.controller.VerTemasController;
 import es.jab.persistence.model.entities.Tema;
 import es.jab.persistence.model.entities.Valoracion;
@@ -10,19 +12,18 @@ public class VerTemasControllerWs implements VerTemasController {
 
 	@Override
 	public List<Tema> recuperarTemas() {
-		// TODO Auto-generated method stub
-		return null;
+		GenericType <List<Tema>> genericType = new GenericType<List<Tema>>() {};
+		return ControllerWs.buildWebServiceManager("/temas").entities(genericType);
 	}
 
 	@Override
 	public Tema recuperarTemaPorId(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return ControllerWs.buildWebServiceManager("/temas", String.valueOf(id)).entity(Tema.class);
 	}
 
 	@Override
 	public void guardaValoracion(Valoracion valoracion) {
-		// TODO Auto-generated method stub
+		ControllerWs.buildWebServiceManager("/valoraciones").create(valoracion);
 
 	}
 
