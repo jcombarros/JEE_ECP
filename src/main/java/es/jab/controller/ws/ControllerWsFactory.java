2ecc9f0d0ed75f8707bdabec0dda204a6928a6b1
Javier Combarros <Javier@138.4.192.251>
2015-03-26 19:12:34 +0100

    ControllerWsFactory class modified. Specific Controller properties added

--- src/main/java/es/jab/controller/ws/ControllerWsFactory.java
+++ src/main/java/es/jab/controller/ws/ControllerWsFactory.java
@@ -1,35 +1,63 @@
 package es.jab.controller.ws;
 
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
 import es.jab.controller.BorraTemaController;
 import es.jab.controller.ControllerFactory;
 import es.jab.controller.NuevoTemaController;
 import es.jab.controller.VerTemasController;
 import es.jab.controller.VerVotacionesController;
+import es.jab.controller.ws.BorraTemaControllerWs;
+import es.jab.controller.ws.NuevoTemaControllerWs;
+import es.jab.controller.ws.VerTemasControllerWs;
+import es.jab.controller.ws.VerVotacionesControllerWs;
 
+@ManagedBean(name = "controllerWsFactory")
+@ApplicationScoped
 public class ControllerWsFactory extends ControllerFactory {
+	
+	private VerTemasController verTemasController;
+	
+	private NuevoTemaController nuevoTemaController;
+	
+	private BorraTemaController borraTemaController;
+	
+	private VerVotacionesController verVotacionesController;
+	
+	public ControllerWsFactory() {
+	}
 
 	@Override
 	public VerTemasController getVerTemasController() {
-		// TODO Auto-generated method stub
-		return null;
+		if (verTemasController == null) {
+			verTemasController = new VerTemasControllerWs();
+        }
+        return verTemasController;
 	}
 
 	@Override
 	public NuevoTemaController getNuevoTemaController() {
-		// TODO Auto-generated method stub
-		return null;
+		if (nuevoTemaController == null) {
+			nuevoTemaController = new NuevoTemaControllerWs();
+        }
+        return nuevoTemaController;
 	}
 
 	@Override
 	public BorraTemaController getBorraTemaController() {
-		// TODO Auto-generated method stub
-		return null;
+		if (borraTemaController == null) {
+			borraTemaController = new BorraTemaControllerWs();
+        }
+        return borraTemaController;
 	}
 
 	@Override
 	public VerVotacionesController getVerVotacionesController() {
-		// TODO Auto-generated method stub
-		return null;
+		if (verVotacionesController == null) {
+			verVotacionesController = new VerVotacionesControllerWs();
+        }
+        return verVotacionesController;
 	}
 
 }
